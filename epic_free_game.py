6149a4a054b13b53073e322b1ed62f716132204d
Sahil <sahiljahagirdar91@gmail.com>
2025-12-22 10:52:31 +0530

    FIXED BUGS FOR TELEGRAM [1]

--- epic_free_game.py
+++ epic_free_game.py
@@ -78,7 +78,7 @@ def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
     payload = {
         "chat_id": TELEGRAM_CHAT_ID,
-        "text": message,
+        "text": message
     }
     response = requests.post(url, json=payload)
     response.raise_for_status()
